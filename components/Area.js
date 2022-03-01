e1d10d705b254bc6d94ea70f005f8817a153620d
Hervé Duchesne <hduchesne@jahia.com>
2022-03-01 19:03:55 +0100

    area use extended graphQL API bug fix apollo cache

--- components/Area.js
+++ components/Area.js
@@ -3,34 +3,63 @@ import Head from "next/head"
 import {useContext} from "react";
 import {JahiaCtx} from "../lib/context";
 
-const Area = ({mainResourcePath, path}) => {
+const Area = ({name, mainResourcePath, path}) => {
     const {workspace} = useContext(JahiaCtx)
-    const getRenderedContent = gql`query($workspace: Workspace!, $path: String!, $mainResourcePath: String) {
-        jcr(workspace: $workspace) {
-            nodeByPath(path: $path) {
-                renderedContent(mainResourcePath: $mainResourcePath, isEditMode:true) {
-                    output
-                    css: staticAssets(type: "css") {
-                        key
-                    }
-                    javascript: staticAssets(type: "javascript") {
-                        key
-                    } 
-                }
+    const getRenderedContent = gql`query ($mainResourcePath: String, $path: String, $language: String, $node: InputJCRNode, $isEditMode: Boolean) {
+        npm {
+            renderedComponent(
+                mainResourcePath:$mainResourcePath,
+                path: $path,
+                language: $language,
+                view: "default",
+                templateType: "html",
+                node: $node,
+                isEditMode: $isEditMode) {
+                id
+                output
             }
         }
-    }`;
+    }`
 
-    const ret = useQuery(getRenderedContent, {variables: {path, mainResourcePath, workspace}})
+    // const getRenderedContent = gql`query($workspace: Workspace!, $path: String!, $mainResourcePath: String) {
+    //     jcr(workspace: $workspace) {
+    //         nodeByPath(path: $path) {
+    //             renderedContent(mainResourcePath: $mainResourcePath, isEditMode:true) {
+    //                 output
+    //                 css: staticAssets(type: "css") {
+    //                     key
+    //                 }
+    //                 javascript: staticAssets(type: "javascript") {
+    //                     key
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }`;
+
+    const area = useQuery(getRenderedContent, {
+        variables: {
+            node: {
+                name,
+                primaryNodeType: "jnt:area"
+            },
+            language: "en",
+            path:"/",
+            mainResourcePath,
+            isEditMode: true
+        }
+    })
+    console.log("[Area] area :",area);
+    console.log("[Area] output :",area?.data?.npm?.renderedComponent?.output);
     return (
         <>
-            <Head>
-            {ret.data && ret.data.jcr.nodeByPath.renderedContent.css && ret.data.jcr.nodeByPath.renderedContent.css.map(s => (
-                <link key={s.key} href={'http://localhost:8080' + s.key} rel="stylesheet" />
-            ))}
-            </Head>
+            {/*<Head>*/}
+            {/*{area.data && area.data.jcr.nodeByPath.renderedContent.css && area.data.jcr.nodeByPath.renderedContent.css.map(s => (*/}
+            {/*    <link key={s.key} href={'http://localhost:8080' + s.key} rel="stylesheet" />*/}
+            {/*))}*/}
+            {/*</Head>*/}
 
-            <div dangerouslySetInnerHTML={{__html: ret.data && ret.data.jcr.nodeByPath.renderedContent.output}}/>
+            <div dangerouslySetInnerHTML={{__html: area.data && area.data.npm.renderedComponent.output}}/>
         </>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -4,23 +4,23 @@ const Layout = ({children,meta}) => {
     return(
         <>
             <Head>
-                <title>{meta.title}</title>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Oxygen:400,700" />
-                <link rel="stylesheet" href="/static/css/animate.css"/>
-                <link rel="stylesheet" href="/static/css/owl.carousel.min.css"/>
-                <link rel="stylesheet" href="/static/css/jquery.fancybox.min.css"/>
-                {/*<link rel="stylesheet" href="/static/font/"/>*/}
-                <link rel="stylesheet" href="/static/css/style.css" />
+                <title>Hello</title>
+                {/*<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Oxygen:400,700" />*/}
+                {/*<link rel="stylesheet" href="/static/css/animate.css"/>*/}
+                {/*<link rel="stylesheet" href="/static/css/owl.carousel.min.css"/>*/}
+                {/*<link rel="stylesheet" href="/static/css/jquery.fancybox.min.css"/>*/}
+                {/*/!*<link rel="stylesheet" href="/static/font/"/>*!/*/}
+                {/*<link rel="stylesheet" href="/static/css/style.css" />*/}
 
             </Head>
             {children}
-            <Script src="/static/js/jquery-3.2.1.min.js" strategy="beforeInteractive" />
-            <Script src="/static/js/popper.min.js"  />
-            <Script src="/static/js/bootstrap.min.js"  />
-            <Script src="/static/js/owl.carousel.min.js"  />
-            <Script src="/static/js/jquery.waypoints.min.js"  />
-            <Script src="/static/js/jquery.fancybox.min.js"  />
-            <Script src="/static/js/main.js"  />
+            {/*<Script src="/static/js/jquery-3.2.1.min.js" strategy="beforeInteractive" />*/}
+            {/*<Script src="/static/js/popper.min.js"  />*/}
+            {/*<Script src="/static/js/bootstrap.min.js"  />*/}
+            {/*<Script src="/static/js/owl.carousel.min.js"  />*/}
+            {/*<Script src="/static/js/jquery.waypoints.min.js"  />*/}
+            {/*<Script src="/static/js/jquery.fancybox.min.js"  />*/}
+            {/*<Script src="/static/js/main.js"  />*/}
         </>
     )
 }
