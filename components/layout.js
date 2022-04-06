e4e23b61578c91c4eb28d5b40df57426e3f90a7d
Thomas Draier <tdraier@jahia.com>
2022-04-06 10:02:13 +0200

    Use sitekey from settings

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ import Script from "next/script";
 import {JahiaCtx} from "../lib/context";
 import classNames from "classnames";
 import styles from "./layout.module.css";
+import config from "../jahia";
 const Layout = ({children,path,templateName,meta}) => {
     const {isEditMode} = React.useContext(JahiaCtx);
 console.log("[Layout] isEditMode : ",isEditMode);
@@ -27,7 +28,7 @@ console.log("[Layout] isEditMode : ",isEditMode);
                 <header role="banner">
                     {/*TODO find a way to get base from context*/}
                     {/*<Nav base={`/sites/${process.env.JAHIA_SITE}/home`} path={path}/>*/}
-                    <Nav base={`/sites/headless-industrial/home`} path={path}/>
+                    <Nav base={`/sites/${config.siteName}/home`} path={path}/>
                 </header>
                 <div className={classNames("top-shadow",{[styles.topShadowEdit] : isEditMode})} ></div>
                 {children}
