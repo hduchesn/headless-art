dbbaae5324fd32a2c26dd1a7b34c2277899e6693
Thomas Draier <tdraier@jahia.com>
2022-04-06 10:02:55 +0200

    Use sitekey from settings

--- jahia.js
+++ jahia.js
@@ -1,5 +1,6 @@
 export default {
     baseUrl: 'http://localhost:8080',
+    siteName: 'headless-industrial',
     token: 'V+InkyRlQ/65oZIJ1TEFcYcfgC1Wyk7LE81s9v84CY0=',
     cnd_type : {
         WIDEN_IMAGE:"wdennt:image",
