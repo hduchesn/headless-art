5ee6707c72c780ba99aa2a8f660722ef49e0306e
Thomas Draier <tdraier@jahia.com>
2022-04-27 14:02:49 +0200

    Fixed uuid generation

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 //TODO get file destination from jahia.js
 module.exports = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
