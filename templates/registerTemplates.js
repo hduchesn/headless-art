140b77a41407b8c522de6985a7579d147b135a34
Hervé Duchesne <hduchesne@jahia.com>
2022-07-01 15:46:45 +0200

    log templates in registerTemplates.js
    TODO review grid creation to create the area from react and not from jahia/jsp

--- templates/registerTemplates.js
+++ templates/registerTemplates.js
@@ -3,9 +3,11 @@ import FixedStructure from "./FixedStructure";
 import {templates} from "@jahia/nextjs-sdk";
 
 export const registerTemplates = () => {
+    console.log('[registerTemplates] templates BEFORE assign : ', templates)
     Object.assign(templates, {
         'open': Open,
         'fixedstructure': FixedStructure,
         'default': Open
     });
+    console.log('[registerTemplates] templates AFTER assign : ', templates)
 }
