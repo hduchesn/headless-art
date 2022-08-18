ab552163af90082badaa113201d5db9e3264bab5
Hervé Duchesne <hduchesne@jahia.com>
2022-08-18 18:14:02 +0200

    update community lib to resolve jss bug, especially with the bubble image. update sdk to use an env variable to resolve ISR time update next.config to use env variable to configure Locales and defaultLocale

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -4,3 +4,12 @@ import {JahiaPage} from '@jahia/nextjs-sdk';
 export default JahiaPage.Component;
 export const getStaticPaths = JahiaPage.getStaticPaths;
 export const getStaticProps = JahiaPage.getStaticProps;
+
+// Export const getStaticProps = async context => {
+//     const data = await JahiaPage.getStaticProps(context);
+//     return {
+//         ...data,
+//         revalidate: parseInt(process.env.NEXT_ISR_TIME, 10),
+//     };
+// };
+
