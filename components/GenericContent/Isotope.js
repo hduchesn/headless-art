8a41f86a3554ddc38d975932b5b7195229f99bbe
Hervé Duchesne <hduchesne@jahia.com>
2022-11-02 18:15:08 +0100

    use next image for isotope view. nextjs cache works when it wants...

--- components/GenericContent/Isotope.js
+++ components/GenericContent/Isotope.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {JahiaCtx, useNode, DefaultImage} from '@jahia/nextjs-sdk';
+import {JahiaCtx, useNode, DefaultImage, getImageURI} from '@jahia/nextjs-sdk';
 import * as PropTypes from 'prop-types';
 import {LinkTo} from '../LinkTo';
 import styles from './isotope.module.css';
+import Image from 'next/image';
 
 export function Isotope({id}) {
-    const {locale} = React.useContext(JahiaCtx);
+    const {workspace, locale, isPreview, isEditMode} = React.useContext(JahiaCtx);
 
     const {data, error, loading} = useNode(id, ['mediaNodeFocus']);
 
@@ -20,13 +21,36 @@ export function Isotope({id}) {
 
     const {path, properties: {mediaNodeFocus: mediaNode}} = data;
 
-    return (
-        <div className={styles.singlePortfolioContent}>
-            {mediaNode
-                && <DefaultImage
+    const getImageComponent = () => {
+        if (mediaNode && (workspace !== 'LIVE' && isEditMode)) {
+            return (
+                <DefaultImage
                     path={mediaNode.path}
                     alt={mediaNode.name}
-                />}
+                />
+            );
+        }
+
+        if (mediaNode && (workspace === 'LIVE' || isPreview)) {
+            return (
+                <Image
+                    unoptimized={isPreview}
+                    src={process.env.NEXT_PUBLIC_JAHIA_BASE_URL + getImageURI({uri: mediaNode.path, workspace})}
+                    alt={mediaNode.name}
+                    // Layout="fill"
+                    objectFit="cover"
+                    width={600}
+                    height={600}
+                />
+            );
+        }
+
+        return null;
+    };
+
+    return (
+        <div className={styles.singlePortfolioContent} style={{position: 'relative'}}>
+            {getImageComponent()}
             <div className={styles.hoverContent}>
                 <LinkTo content={{linkType: 'self', linkTarget: '_self', path}} locale={locale} className="portfolio-img">
                     +
